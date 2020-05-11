bb7f9cd0ad83330169164ca627a9160473fa1c74
Baiyuan Wang <bywang@thoughtworks.com>
2020-05-11 22:20:25 +0800

    fix banner image

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@
         path = require('path'),
         paths = {
             root: './',
-            source: './source/' //主题下原文件
+            source: './themes/hexo-theme-snippet/source/' //主题下原文件
         }
 
     /*====================================================
