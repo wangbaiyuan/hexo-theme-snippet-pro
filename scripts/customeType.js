7d74d23ac1411b5722231407574b1656d07da146
Baiyuan Wang <bywang@thoughtworks.com>
2020-05-17 19:19:19 +0800

    adjust new style

--- scripts/customeType.js
+++ scripts/customeType.js
@@ -3,7 +3,10 @@ var pagination = require('hexo-pagination');
 hexo.extend.generator.register('moment', function (locals) {
     return pagination('moment', locals.data.news, {
         perPage: 10,
-        layout: ['_partial/news'],
-        data: {}
+        layout: ['_customizedLayout/news'],
+        data: {
+            title: "业界动态",
+            description: "与时俱进，追踪最新技术动态",
+        }
     });
 });
